731ec3a1719bcef8075b7ef84a74a69698993418
Antonin <antonin.pire@gmail.com>
2023-10-03 22:41:53 +0200

    ui: improve landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "@/app/dashboard/_components/theme-toggle";
 import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/lucia";
+import { ArrowRight } from "lucide-react";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -25,6 +26,42 @@ export default async function Home() {
         <h4 className="text-2xl font-medium">
           A lightweight analytics solution for all your websites.
         </h4>
+        <div className="flex justify-center">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/antoninpire/analytics"
+          >
+            <button className="px-3 py-1.5 text-sm rounded-full border border-primary inline-flex items-center">
+              See on github
+              <ArrowRight size={18} className="ml-1" />
+            </button>
+          </a>
+        </div>
+      </div>
+      <div
+        aria-hidden="true"
+        className="dark:hidden pointer-events-none absolute inset-x-0 -z-10 transform-gpu overflow-hidden blur-3xl top-[3vh]"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+        />
+      </div>
+      <div
+        aria-hidden="true"
+        className="dark:hidden pointer-events-none absolute right-0 -z-10 transform-gpu overflow-hidden blur-3xl top-[3vh] max-w-[40vw]"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+          className="relative aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#a5c7f5] to-[#8789fc] opacity-30 sm:w-[72.1875rem]"
+        />
       </div>
     </main>
   );
